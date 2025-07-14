98475fd58491627c4d8c888a572912ff782fbf0b
phili <philiptelkamp@gmail.com>
2025-07-13 21:13:30 -0700

    Initial attempts at standing up service using containers

--- src/TxTracker.TransactionsApi/Program.cs
+++ src/TxTracker.TransactionsApi/Program.cs
@@ -1,6 +1,10 @@
 using Microsoft.EntityFrameworkCore;
+using TxTracker.TransactionsApi.Accounts;
+using TxTracker.TransactionsApi.Categories;
 using TxTracker.TransactionsApi.Configurations;
 using TxTracker.TransactionsApi.Data;
+using TxTracker.TransactionsApi.Tags;
+using TxTracker.TransactionsApi.Transactions;
 
 namespace TxTracker.TransactionsApi;
 
@@ -10,14 +14,19 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
+
         // Add services to the container.
         builder.Services.AddControllers();
 
         builder.Configuration.Bind(nameof(DbConfig));
         builder.Services.AddDbContext<TransactionsContext>((sp, options) =>
         {
-            options.UseNpgsql(sp.GetRequiredService<DbConfig>().ConnectionString);
+            options.UseNpgsql(builder.Configuration.GetConnectionString("DB_CONN_STRING"));
         });
+        builder.Services.AddScoped<TransactionsRepository>();
+        builder.Services.AddScoped<TagsRepository>();
+        builder.Services.AddScoped<CategoriesRepository>();
+        builder.Services.AddScoped<AccountsRepository>();
 
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
